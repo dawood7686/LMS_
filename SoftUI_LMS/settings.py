6618011b2b853e1296497fb9b9151d4c54fbbea9
Dawood Ahmad <101159975+dawood7686@users.noreply.github.com>
2024-03-16 04:33:58 +0500

    exceptional handling

--- SoftUI_LMS/settings.py
+++ SoftUI_LMS/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-08luofjdml5f&86ba*m2b30dgbv!83trt0#4ljrb6#o)m)*pzr
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1', '*']
 
 
 # Application definition
